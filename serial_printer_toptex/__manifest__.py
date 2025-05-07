b63c5bfd641a985fed1ce27c26ffda9f2d1a9a24
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-07 15:04:41 +0900

    subida modulo toptex

---
+++ serial_printer_toptex/__manifest__.py
@@ -0,0 +1,9 @@
+{
+        "name": "Toptex Serial Printer Integration",
+        "version": "1.0",
+        "summary": "Conexión automática con API de Toptex",
+        "author": "Serial Printer",
+        "depends": ["base", "product", "sale", "purchase"],
+        "data": [],
+        "installable": True
+    }
