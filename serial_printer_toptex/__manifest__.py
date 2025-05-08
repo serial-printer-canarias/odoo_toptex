26912bc94a4e0fecf5e6707c6114a7ac40401ad9
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-08 19:11:12 +0900

    __manifest__.py

--- serial_printer_toptex/__manifest__.py
+++ serial_printer_toptex/__manifest__.py
@@ -1,13 +1,16 @@
 {
     "name": "Toptex Serial Printer Integration",
     "version": "1.0",
-    "summary": "Conexión automática con API de Toptex",
+    "summary": "Sincroniza productos, marcas y tallas desde la API de Toptex.",
+    "description": "Importación de marcas y productos desde Toptex para usarlos en ventas personalizadas.",
     "author": "Serial Printer",
+    "website": "https://serialprintercanarias.com",
+    "category": "Sales",
     "depends": ["base", "product", "sale", "purchase"],
     "data": [
-        
-        "views/toptex_product_views.xml",
-        "views/toptex_brand_import_button.xml"
+        "views/toptex_brand_import_button.xml",
     ],
-    "installable": True
+    "installable": True,
+    "application": True,
+    "auto_install": False,
 }
