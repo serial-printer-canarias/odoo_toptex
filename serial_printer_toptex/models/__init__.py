0f1a9a717eaa4d3722a3aec547dcfec9fa8c7d3b
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-08 11:47:35 +0900

    modelo

--- serial_printer_toptex/models/__init__.py
+++ serial_printer_toptex/models/__init__.py
@@ -1,8 +1,1 @@
 from . import toptex_brand
-from odoo import models, fields
-
-class ToptexBrand(models.Model):
-    _name = 'toptex.brand'
-    _description = 'Toptex Brand'
-
-    name = fields.Char(string="Brand Name", required=True)
