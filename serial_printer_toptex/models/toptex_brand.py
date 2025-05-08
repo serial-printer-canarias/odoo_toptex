c12805d38d5ca0370d1c86feb5b040b89f68ab91
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-08 11:43:05 +0900

    marcas

---
+++ serial_printer_toptex/models/toptex_brand.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class ToptexBrand(models.Model):
+    _name = 'toptex.brand'
+    _description = 'Marca de Toptex'
+
+    name = fields.Char(string='Nombre', required=True)
+    toptex_id = fields.Char(string='ID en Toptex', required=True, index=True)
