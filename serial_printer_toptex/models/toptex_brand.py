93415bb42641d267dc0cbb67e6e65cf14cb4d41d
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-09 11:58:35 +0900

    brands

--- serial_printer_toptex/models/toptex_brand.py
+++ serial_printer_toptex/models/toptex_brand.py
@@ -1,7 +1,4 @@
-import logging
 from odoo import models, fields
-
-_logger = logging.getLogger(__name__)
 
 class ToptexBrand(models.Model):
     _name = 'toptex.brand'
@@ -9,7 +6,3 @@ class ToptexBrand(models.Model):
 
     name = fields.Char(string='Nombre', required=True)
     toptex_id = fields.Char(string='ID en Toptex', required=True, index=True)
-
-    def import_brands(self):
-        _logger.info("Botón de importar marcas presionado.")
-        # Aquí luego se conectará con la API real
