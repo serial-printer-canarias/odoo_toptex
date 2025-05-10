977526dcdacb633e7d1e8f1a691de401add9b5be
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-10 18:51:33 +0200

    Actualizar toptex_brand.py

--- serial_printer_catalog/models/toptex_brand.py
+++ serial_printer_catalog/models/toptex_brand.py
@@ -1,9 +1,8 @@
-
 from odoo import models, fields
 
 class ToptexBrand(models.Model):
     _name = 'toptex.brand'
-    _description = 'Marca del proveedor externo'
+    _description = 'Marca externa'
 
     name = fields.Char(string="Nombre", required=True)
-    toptex_id = fields.Char(string="ID Externo", required=True)
+    toptex_id = fields.Char(string="ID externo")
