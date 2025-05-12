f45b74f95ec8f27109a5e697147e47404f2559f8
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-12 16:50:54 +0100

    Delete serial_printer_toptex directory

--- serial_printer_toptex/models/toptex_product.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ToptexProduct(models.Model):
-    _name = 'toptex.product'
-    _description = 'Producto Toptex'
-
-    name = fields.Char(string="Nombre del producto")
-    reference = fields.Char(string="Referencia")
