0dd3449af296b4330179e74eaf7b7160367e0665
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-12 17:28:46 +0200

    Add files via upload

---
+++ serial_printer_catalog/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Serial Printer Catálogo",
+    "version": "1.0",
+    "depends": ["base"],
+    "author": "Serial Printer",
+    "category": "Tools",
+    "description": "Gestión de marcas externas para catálogo",
+    "data": [
+        "views/toptex_brand_views.xml",
+        "views/toptex_brand_import_button.xml",
+        "views/toptex_menus.xml"
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": True
+}
