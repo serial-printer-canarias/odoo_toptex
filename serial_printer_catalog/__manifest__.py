9cdb00f67726091614f2643d78f0222c17e3e53e
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-13 09:44:28 +0200

    Add files via upload

---
+++ serial_printer_catalog/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Catálogo de marcas externas',
+    'version': '1.0',
+    'summary': 'Importación de marcas externas para catálogo',
+    'depends': ['base'],
+    'data': [
+        'views/toptex_brand_views.xml',
+        'views/toptex_brand_import_button.xml',
+        'views/toptex_menus.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
