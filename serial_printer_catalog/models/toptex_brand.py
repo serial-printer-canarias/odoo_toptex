65fc86d7662d35353da2f7097ae6a913ac8664d6
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-13 12:05:03 +0100

    Delete serial_printer_catalog directory

--- serial_printer_catalog/models/toptex_brand.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ToptexBrand(models.Model):
-    _name = 'toptex.brand'
-    _description = 'Marca externa'
-
-    name = fields.Char(string='Nombre', required=True)
-    toptex_id = fields.Integer(string='ID externo', required=True)
