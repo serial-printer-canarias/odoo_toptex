10fc009ce81b96601d12c55a6656ecefdd85e270
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-15 16:15:19 +0100

    api

---
+++ addons/serial_printer_catalog/__manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    "name": "Serial Printer Catalog",
+    "version": "1.0",
+    "summary": "Catálogo de productos y marcas importados por API",
+    "description": "Importa productos, marcas, variantes y precios desde una API externa.",
+    "author": "Serial Printer",
+    "category": "Sales",
+    "website": "https://www.serialprinter.com",
+    "depends": ["base"],
+    "data": [
+        "views/brand_views.xml",
+        "views/product_views.xml",
+        "views/menu.xml"
+    ],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3"
+}
