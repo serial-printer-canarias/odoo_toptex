2ee9ff4d90bbc1dd480f6122a5433b31556a5b66
Marc Bafafaluy <marcbafaluy@gmail.com>
2025-05-15 15:51:17 +0100

    delete

--- serial_printer_toptex/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import toptex_brand
--- serial_printer_toptex/models/toptex_brand.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class ToptexBrand(models.Model):
-    _name = 'toptex.brand'
-    _description = 'Marca de Toptex'
-
-    name = fields.Char(string='Nombre', required=True)
-    toptex_id = fields.Char(string='ID Toptex', required=True)
