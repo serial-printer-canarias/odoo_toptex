b3ea36ca0a285a7e5780d314ed9465f60d5fdb35
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-20 19:03:55 +0100

    Actualizar attribute.py

--- addons/serial_printer_catalog/models/attribute.py
+++ addons/serial_printer_catalog/models/attribute.py
@@ -2,16 +2,19 @@ import requests
 from odoo import models, fields, api
 
 class SerialPrinterAttribute(models.Model):
-    _name = 'serial.printer.attribute'
-    _description = 'Atributos del catálogo'
+    _name = "serial.printer.attribute"
+    _description = "Atributo de producto (como talla o color)"
 
-    name = fields.Char(string='Nombre', required=True)
-    toptex_id = fields.Integer(string='ID en TopTex', required=True, unique=True)
-    type = fields.Selection([('size', 'Talla'), ('color', 'Color')], string='Tipo')
+    name = fields.Char(string="Nombre", required=True)
+    toptex_id = fields.Integer(string="ID en TopTex", required=True, unique=True)
+    attribute_type = fields.Selection([
+        ('size', 'Talla'),
+        ('color', 'Color')
+    ], string="Tipo de atributo")
 
     @api.model
     def sync_attributes_from_api(self):
-        url = "https://api.toptex.io/api/attributes"
+        url = "https://api.toptex.io/api/products/attributes"
         headers = {
             "x-api-key": "qh7SERVyz43xDDNaRoNs0aLxGnTtfSOX4bOvgizE"
         }
@@ -20,20 +23,18 @@ class SerialPrinterAttribute(models.Model):
         if response.status_code == 200:
             attributes = response.json()
             for attr in attributes:
-                self.env['serial.printer.attribute'].sudo().update_or_create_attribute(attr)
+                self.env["serial.printer.attribute"].sudo().update_or_create_attribute(attr)
         else:
             raise Exception(f"Error {response.status_code}: {response.text}")
 
     @api.model
     def update_or_create_attribute(self, attr):
-        attr_type = 'size' if attr.get('code') == 'size' else 'color'
-        existing = self.search([('toptex_id', '=', attr.get('id'))], limit=1)
+        existing = self.search([("toptex_id", "=", attr.get("id"))], limit=1)
         values = {
             "name": attr.get("name"),
             "toptex_id": attr.get("id"),
-            "type": attr_type
+            "attribute_type": attr.get("type"),
         }
-
         if existing:
             existing.write(values)
         else:
