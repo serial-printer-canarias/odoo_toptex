8ab06462f929b822c4b3bc6dfebfaaa862b84f5e
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-20 07:52:51 +0100

    Actualizar attribute.py

--- addons/serial_printer_catalog/models/attribute.py
+++ addons/serial_printer_catalog/models/attribute.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class SerialPrinterAttribute(models.Model):
     _name = 'serial.printer.attribute'
-    _description = 'Atributo de producto'
+    _description = 'Atributos de productos Serial Printer'
 
-    name = fields.Char(string='Nombre', required=True)
-    code = fields.Char(string='Código')
+    name = fields.Char(string='Nombre del atributo', required=True)
+    code = fields.Char(string='Código del atributo')
