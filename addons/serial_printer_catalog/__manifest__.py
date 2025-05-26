490da2169a37c108ab0cee51b3b5c1cc75de961d
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-26 16:51:14 +0100

    Actualizar __manifest__.py

--- addons/serial_printer_catalog/__manifest__.py
+++ addons/serial_printer_catalog/__manifest__.py
@@ -1,31 +1,48 @@
 {
-    'name': 'Catálogo Serial Printer',
-    'version': '1.0',
-    'summary': 'Sincronización con API TopTex',
-    'sequence': 10,
-    'description': "Importa productos, variantes y stock desde la API",
+    'name': 'Catálogo',
+    'version': '1.0.0',
     'category': 'Sales',
-    'author': 'Serial Printer',
-    'license': 'LGPL-3',
-    'depends': ['base', 'product', 'sale'],
+    'summary': 'Catálogo de productos con integración API',
+    'description': 'Sincroniza productos, marcas, variantes e imágenes desde la API de TopTex.',
+    'depends': ['base', 'sale', 'product'],
     'data': [
+        # Menú raíz
         'views/menu_root.xml',
+
+        # Marcas
         'views/menu_brand.xml',
-        'views/menu_attribute.xml',
-        'views/menu_variant.xml',
-        'views/menu_product.xml',
-        'views/token_views.xml',
-
         'views/brand_views.xml',
-        'views/attribute_views.xml',
-        'views/variant_views.xml',
-        'views/product_views.xml',
-
         'data/cron_brand.xml',
-        'data/cron_attribute.xml',
-        'data/cron_variant.xml',
+
+        # Productos
+        'views/menu_product.xml',
+        'views/product_views.xml',
         'data/cron_product.xml',
+
+        # Atributos y variantes
+        'views/menu_attribute.xml',
+        'views/attribute_views.xml',
+        'data/cron_attribute.xml',
+
+        'views/menu_variant.xml',
+        'views/variant_views.xml',
+        'data/cron_variant.xml',
+
+        # Imágenes
+        'data/cron_image.xml',
+
+        # Precios
+        'views/menu_prices.xml',
+        'views/prices_views.xml',
+        'data/cron_prices.xml',
+
+        # Stock
         'data/cron_stock.xml',
+
+        # Token
+        'views/menu_token.xml',
+        'views/token_views.xml',
+        'data/cron_token.xml',
     ],
     'installable': True,
     'application': True,
