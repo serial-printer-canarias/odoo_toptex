09c1d04beaee79e4b35b0e7749040b4a1c13511a
serial-printer-canarias <marcbafaluy@gmail.com>
2025-05-29 13:37:52 +0100

    Actualizar product.py

--- addons/serial_printer_catalog/models/product.py
+++ addons/serial_printer_catalog/models/product.py
@@ -1,44 +1,47 @@
 import requests
 import base64
 import os
-
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
 class SerialPrinterProduct(models.Model):
-    _name = 'serial_printer_product'
+    _name = 'serial.printer.product'
     _description = 'Producto del catálogo'
 
-    name = fields.Char(string='Nombre')
-    toptex_id = fields.Char(string='ID TopTex')
-    reference = fields.Char(string='Referencia')
-    description = fields.Text(string='Descripción')
-    image = fields.Binary(string='Imagen')
+    name = fields.Char(string="Nombre")
+    toptex_id = fields.Char(string="ID TopTex")
+    reference = fields.Char(string="Referencia")
+    description = fields.Text(string="Descripción")
+    image = fields.Binary(string="Imagen")
 
     def _get_toptex_credential(self, key):
-        param = self.env['ir.config_parameter'].sudo().get_param(key)
-        if not param:
+        param = self.env['ir.config_parameter'].sudo()
+        value = param.get_param(key)
+        if not value:
             raise UserError(f'Falta el parámetro de configuración: {key}')
-        return param
+        return value
 
     def _generate_token(self):
         proxy_url = self._get_toptex_credential('toptex_proxy_url')
-        auth_url = "https://api.toptex.io/v3/authenticate"
+        auth_url = 'https://api.toptex.io/v3/authenticate'
+
         headers = {
-            "x-api-key": self._get_toptex_credential('toptex_api_key'),
-            "Accept-Encoding": "identity",
-            "Accept": "application/json",
+            'x-api-key': self._get_toptex_credential('toptex_api_key'),
+            'Accept': 'application/json'
         }
+
         data = {
-            "username": self._get_toptex_credential('toptex_username'),
-            "password": self._get_toptex_credential('toptex_password'),
+            'username': self._get_toptex_credential('toptex_username'),
+            'password': self._get_toptex_credential('toptex_password')
         }
+
         response = requests.post(
             proxy_url,
-            params={"url": auth_url},
+            params={'url': auth_url},
             headers=headers,
             json=data
         )
+
         if response.status_code == 200:
             return response.json().get('token')
         else:
@@ -47,20 +50,25 @@ class SerialPrinterProduct(models.Model):
     @api.model
     def sync_products_from_api(self):
         proxy_url = self._get_toptex_credential('toptex_proxy_url')
+        catalog_url = 'https://api.toptex.com/v3/products/all?usage_right=b2b_uniquement&result_in_file=1'
         token = self._generate_token()
 
-        url = "https://api.toptex.com/v3/products/all?usage_right=b2b_uniquement&result_in_file=1"
         headers = {
-            "x-api-key": self._get_toptex_credential('toptex_api_key'),
-            "x-toptex-authorization": token,
-            "Accept-Encoding": "identity",
-            "Accept": "application/json"
+            'x-api-key': self._get_toptex_credential('toptex_api_key'),
+            'x-toptex-authorization': token,
+            'Accept': 'application/json',
+            'Accept-Encoding': 'identity',  # evita gzip
         }
 
-        response = requests.get(proxy_url, params={"url": url}, headers=headers)
+        response = requests.get(
+            proxy_url,
+            params={'url': catalog_url},
+            headers=headers
+        )
+
         if response.status_code == 200:
-            catalog = response.json().get('items', [])
-            for product_data in catalog:
+            catalog = response.json()
+            for product_data in catalog.get('items', []):
                 self._create_or_update_product(product_data)
         else:
             raise UserError(f'Error al obtener catálogo ({response.status_code}): {response.text}')
@@ -83,7 +91,7 @@ class SerialPrinterProduct(models.Model):
             'name': product_data.get('name', ''),
             'toptex_id': toptex_id,
             'reference': product_data.get('reference'),
-            'description': product_data.get('description', ''),
+            'description': product_data.get('description'),
             'image': image_binary,
         }
 
