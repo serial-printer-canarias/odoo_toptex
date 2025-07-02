14f0272280d3fdedf3f4d83fdd67be24d2d2e826
serial-printer-canarias <marcbafaluy@gmail.com>
2025-07-02 21:55:05 +0100

    Actualizar __manifest__.py

--- addons/serial_printer_catalog/__manifest__.py
+++ addons/serial_printer_catalog/__manifest__.py
@@ -2,13 +2,14 @@
     'name': 'Catálogo TopTex',
     'version': '1.0.0',
     'category': 'Sales',
-    'summary': 'Importación de productos TopTex',
-    'description': 'Crea productos desde la API de TopTex en el modelo estándar de Odoo',
+    'summary': 'Importación de productos TopTex para Odoo',
+    'description': 'Crea productos y variantes desde la API de TopTex',
     'author': 'Serial Printer',
     'license': 'LGPL-3',
     'depends': ['base', 'product'],
     'data': [
-        'data/cron_product.xml'
+        'ir.model.access.csv',
+        # 'data/cron_product.xml',  # Solo si usas el cron, si no, bórralo o comenta
     ],
     'installable': True,
     'application': False,
