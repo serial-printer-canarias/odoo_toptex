150ac3989ff0e246f5973e48168e3e95dcdb0e20
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 16:56:26 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,25 +1,18 @@
 {
     'name': 'Serial Printer Custom Wizard',
-    'version': '1.1.0',
-    'summary': 'Gestión de personalización + botón en eCommerce',
+    'version': '1.1.1',
+    'summary': 'Botón “Personalizar” en ficha de producto + ruta web de prueba',
     'category': 'Sales/Customization',
     'author': 'Serial Printer Fuerteventura',
     'website': 'https://serial-printer.com',
     'license': 'LGPL-3',
     'depends': ['base', 'product', 'sale', 'website', 'website_sale'],
     'data': [
-        # Backoffice
         'security/ir.model.access.csv',
         'views/personalizacion_wizard_views.xml',
-        # Página web muy simple para probar la ruta
         'views/personalizacion_wizard_website_views.xml',
+        'views/assets.xml',  # << forzamos la carga del JS en frontend
     ],
-    'assets': {
-        # Solo un JS pequeño que inserta el botón en la ficha de producto
-        'web.assets_frontend': [
-            'serial_printer_custom_wizard/static/src/js/add_customize_button.js',
-        ],
-    },
     'installable': True,
     'application': False,
 }
