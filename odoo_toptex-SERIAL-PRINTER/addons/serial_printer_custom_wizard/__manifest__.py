8e143e0950c8a104f8e04d0e2b184d7a6ec6d64c
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-10 00:13:37 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,21 +1,20 @@
 {
     'name': 'Serial Printer Custom Wizard',
     'summary': 'Personalización de productos en la web con previsualización',
-    'version': '1.0.0',
+    'version': '1.0.1',
     'category': 'Website',
     'license': 'LGPL-3',
     'author': 'Serial Printer Fuerteventura',
     'website': 'https://serial-printer.com',
     'depends': ['website', 'website_sale', 'sale', 'portal'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/personalizacion_wizard_views.xml',
         'views/personalizacion_wizard_website_views.xml'
     ],
     'assets': {
         'web.assets_frontend': [
             'serial_printer_custom_wizard/static/src/js/add_customize_button.js',
             'serial_printer_custom_wizard/static/src/js/personalizar_preview.js',
+            'serial_printer_custom_wizard/static/src/js/personalizar_meta_patch.js',
             'serial_printer_custom_wizard/static/src/css/personalizar_preview.css'
         ]
     },
