a5c0fd831e5c366d3cb275e7148fee1fca36cb95
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 17:14:03 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,17 +1,16 @@
 {
     'name': 'Serial Printer Custom Wizard',
-    'version': '1.1.1',
-    'summary': 'Botón “Personalizar” en ficha de producto + ruta web de prueba',
-    'category': 'Sales/Customization',
+    'version': '1.1.2',
+    'summary': 'Botón “Personalizar” en la ficha de producto',
+    'category': 'Website/Shop',
     'author': 'Serial Printer Fuerteventura',
     'website': 'https://serial-printer.com',
     'license': 'LGPL-3',
-    'depends': ['base', 'product', 'sale', 'website', 'website_sale'],
+    'depends': ['base', 'product', 'website', 'website_sale'],
     'data': [
         'security/ir.model.access.csv',
-        'views/personalizacion_wizard_views.xml',
+        'views/assets.xml',                           # << fuerza el JS
         'views/personalizacion_wizard_website_views.xml',
-        'views/assets.xml',  # << forzamos la carga del JS en frontend
     ],
     'installable': True,
     'application': False,
