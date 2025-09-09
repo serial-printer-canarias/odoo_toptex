e44562c48eb7d2c94aca765013ac0ba4cd9248fc
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 16:39:37 +0100

    Actualizar __init__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/__init__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import product_personalizacion
-from . import personalizacion_wizard  # si ya lo tienes, mantenlo; si no existe, no pasa nada
+# NO importes aquí otros wizards/modelos hasta estabilizar
