dc5888d20bb4ad31181c4e78b549fbe1ab0ce122
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 17:15:44 +0100

    Actualizar main.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
@@ -2,10 +2,12 @@ from odoo import http
 from odoo.http import request
 
 class SerialPrinterWebsite(http.Controller):
-
     @http.route('/personalizar/<int:product_tmpl_id>', type='http', auth='public', website=True)
     def personalizar_producto(self, product_tmpl_id, **kw):
         product = request.env['product.template'].sudo().browse(product_tmpl_id)
         if not product.exists():
             return request.not_found()
-        return request.render('serial_printer_custom_wizard.website_personalizar_page', {'product': product})
+        return request.render(
+            'serial_printer_custom_wizard.website_personalizar_page',
+            {'product': product}
+        )
