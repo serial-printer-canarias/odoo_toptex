fdb251974da0be842ce49d6c1583e3ba44dd07a9
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 23:47:03 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,39 +1,24 @@
-# -*- coding: utf-8 -*-
 {
     'name': 'Serial Printer Custom Wizard',
-    'version': '1.0',
+    'summary': 'Personalización de productos en la web con previsualización',
+    'version': '1.0.0',
     'category': 'Website',
-    'summary': 'Personalizador sencillo con preview en la web',
-    'description': '''
-Permite al cliente subir logo, escoger técnica/posición/color, ver una previsualización
-sobre la imagen del producto y enviar los datos al carrito / al taller.
-''',
+    'license': 'LGPL-3',
     'author': 'Serial Printer Fuerteventura',
     'website': 'https://serial-printer.com',
-    'depends': [
-        'website',
-        'website_sale',
-        'sale',
-        'portal',
-        'mail',
-    ],
+    'depends': ['website', 'website_sale', 'sale', 'portal'],
     'data': [
         'security/ir.model.access.csv',
         'views/personalizacion_wizard_views.xml',
-        'views/personalizacion_wizard_website_views.xml',
-        'views/website_customize_button.xml',
-        'report/mockup_personalizacion_template.xml',
+        'views/personalizacion_wizard_website_views.xml'
     ],
     'assets': {
         'web.assets_frontend': [
-            # JS del botón en la ficha (si lo estás usando)
             'serial_printer_custom_wizard/static/src/js/add_customize_button.js',
-            # NUEVOS assets del previsualizador
-            'serial_printer_custom_wizard/static/src/js/customizer_preview.js',
-            'serial_printer_custom_wizard/static/src/css/customizer.css',
-        ],
+            'serial_printer_custom_wizard/static/src/js/personalizar_preview.js',
+            'serial_printer_custom_wizard/static/src/css/personalizar_preview.css'
+        ]
     },
     'installable': True,
-    'application': True,
-    'license': 'LGPL-3',
+    'application': False
 }
