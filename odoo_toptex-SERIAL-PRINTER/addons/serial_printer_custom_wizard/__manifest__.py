03c8c23e978abd7dbee0b1e17740e145edefd0c5
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-10 00:43:12 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,23 +1,22 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Serial Printer Custom Wizard',
-    'summary': 'Personalización de productos en la web con previsualización',
-    'version': '1.0.1',
-    'category': 'Website',
-    'license': 'LGPL-3',
-    'author': 'Serial Printer Fuerteventura',
-    'website': 'https://serial-printer.com',
-    'depends': ['website', 'website_sale', 'sale', 'portal'],
+    'name': 'Serial Printer - Custom Wizard',
+    'version': '0.2.0',  # <-- sube versión en cada cambio de assets
+    'category': 'Website/Website',
+    'summary': 'Botón Personalizar y página de personalización',
+    'author': 'Serial Printer',
+    'license': 'OEEL-1',
+    'depends': ['website_sale'],
     'data': [
-        'views/personalizacion_wizard_website_views.xml'
+        'security/ir.model.access.csv',
+        'views/personalizacion_wizard_views.xml',
+        'views/personalizacion_wizard_website_views.xml',
     ],
     'assets': {
         'web.assets_frontend': [
             'serial_printer_custom_wizard/static/src/js/add_customize_button.js',
-            'serial_printer_custom_wizard/static/src/js/personalizar_preview.js',
-            'serial_printer_custom_wizard/static/src/js/personalizar_meta_patch.js',
-            'serial_printer_custom_wizard/static/src/css/personalizar_preview.css'
-        ]
+        ],
     },
     'installable': True,
-    'application': False
+    'application': False,
 }
