8772a8e0b9dc333249bd9237ddbb14b23ba3f678
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 16:49:07 +0100

    Actualizar main.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
@@ -1,22 +1,12 @@
-# -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
 
-class SerialPrinterCustomizer(http.Controller):
+class SerialPrinterWebsite(http.Controller):
 
-    @http.route(['/personalizacion/<int:product_id>'], type='http', auth='public', website=True, csrf=False)
-    def personalizar_producto(self, product_id, **post):
-        product = request.env['product.template'].sudo().browse(product_id)
+    @http.route('/personalizar/<int:product_tmpl_id>', type='http', auth='public', website=True)
+    def personalizar_producto(self, product_tmpl_id, **kw):
+        product = request.env['product.template'].sudo().browse(product_tmpl_id)
         if not product.exists():
             return request.not_found()
-        return request.render('serial_printer_custom_wizard.customize_product_template', {
-            'product': product,
-        })
-
-    @http.route(['/personalizacion/submit/<int:product_id>'], type='http', auth='public', website=True, csrf=False, methods=['POST'])
-    def personalizar_submit(self, product_id, **post):
-        product = request.env['product.template'].sudo().browse(product_id)
-        if not product.exists():
-            return request.not_found()
-        # TODO: guardar datos en tu modelo si lo necesitas
-        return request.redirect('/shop/product/%s' % product_id)
+        values = {'product': product}
+        return request.render('serial_printer_custom_wizard.website_personalizar_page', values)
