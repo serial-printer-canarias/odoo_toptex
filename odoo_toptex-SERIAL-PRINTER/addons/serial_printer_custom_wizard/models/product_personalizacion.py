6ba448e736402c8ac81242394ad34656e5b1ea21
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-09 16:28:27 +0100

    Actualizar product_personalizacion.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/product_personalizacion.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/product_personalizacion.py
@@ -6,33 +6,39 @@ class ProductPersonalizacion(models.Model):
     _description = 'Personalización de producto'
     _order = 'create_date desc'
 
-    # Producto base (lo que pedía la vista)
+    # Producto a personalizar
     product_tmpl_id = fields.Many2one(
         'product.template',
         string='Producto',
         required=True,
-        ondelete='cascade',
+        ondelete='cascade'
     )
 
-    # Datos de personalización
-    logo = fields.Binary(string='Logo', attachment=True)
-    tecnica_personalizacion = fields.Selection([
-        ('dtf', 'DTF'),
-        ('bordado', 'Bordado'),
-        ('serigrafia', 'Serigrafía'),
-        ('vinilo', 'Vinilo'),
-    ], string='Técnica de impresión')
+    # Datos de la personalización
+    logo = fields.Binary(string='Logo')
+    logo_filename = fields.Char(string='Nombre del archivo')
 
-    posicion_diseno = fields.Selection([
-        ('pecho', 'Pecho'),
-        ('espalda', 'Espalda'),
-        ('manga', 'Manga'),
-        ('gorra_frontal', 'Gorra - Frontal'),
-    ], string='Posición del diseño')
+    tecnica_personalizacion = fields.Selection(
+        selection=[
+            ('serigrafia', 'Serigrafía'),
+            ('bordado', 'Bordado'),
+            ('dtf', 'DTF'),
+            ('vinilo', 'Vinilo'),
+        ],
+        string='Técnica de impresión'
+    )
+
+    posicion_diseno = fields.Selection(
+        selection=[
+            ('pecho_izquierdo', 'Pecho izquierdo'),
+            ('pecho_derecho', 'Pecho derecho'),
+            ('frontal', 'Frontal'),
+            ('espalda', 'Espalda'),
+            ('manga', 'Manga'),
+        ],
+        string='Posición'
+    )
 
     color_impresion = fields.Char(string='Color de impresión')
     cantidad = fields.Integer(string='Cantidad', default=1)
     observaciones = fields.Text(string='Observaciones')
-
-    # metadatos
-    create_date = fields.Datetime(string='Creado', readonly=True)
