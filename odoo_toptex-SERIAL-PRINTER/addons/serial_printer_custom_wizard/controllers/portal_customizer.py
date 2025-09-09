d6e20edd6d822b31f1c2c7f44659e76239990884
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-10 00:18:57 +0100

    Actualizar portal_customizer.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/portal_customizer.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/portal_customizer.py
@@ -6,15 +6,9 @@ class SPWPortalCustomizer(http.Controller):
 
     @http.route(['/personalizar/<int:product_id>'], type='http', auth='public', website=True, sitemap=False)
     def personalizar(self, product_id, **kw):
-        Product = request.env['product.template'].sudo()
-        product = Product.browse(product_id)
+        product = request.env['product.template'].sudo().browse(product_id)
         if not product.exists():
             return request.not_found()
-
-        # Pasamos explícitamente el objeto principal al contexto
-        values = {
+        return request.render('serial_printer_custom_wizard.website_personalizar_page', {
             'product': product,
-            'main_object': product,              # <- clave para editor
-            'main_object_name': 'product.template',
-        }
-        return request.render('serial_printer_custom_wizard.website_personalizar_page', values)
+        })
