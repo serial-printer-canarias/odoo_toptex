f1261b1b0bf9f8699b921a5763e88b51907961ee
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-10 15:12:47 +0100

    Actualizar main.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
+# controllers/main.py
 from odoo import http
 from odoo.http import request
 
-class SpwWebsite(http.Controller):
-    @http.route(['/spw/personalizar/<int:tmpl_id>'], type='http', auth="public", website=True, sitemap=False)
-    def spw_personalizar(self, tmpl_id, **kwargs):
-        product = request.env['product.template'].sudo().browse(tmpl_id)
+class SPWController(http.Controller):
+
+    @http.route(['/personalizar/<int:product_id>'], type='http', auth='public', website=True, sitemap=False)
+    def personalize(self, product_id, **kw):
+        product = request.env['product.template'].sudo().browse(product_id)
         if not product.exists():
             return request.not_found()
-        return request.render('serial_printer_custom_wizard.website_personalizar_page', {
-            'product': product,
-            'return_url': product.website_url or '/shop',
-        })
+        values = {
+            "product": product,
+            "main_object": product,   # <- esto evita el error del editor
+        }
+        return request.render("serial_printer_custom_wizard.website_personalizar_page", values)
