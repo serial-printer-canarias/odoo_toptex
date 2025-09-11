c0e530c196cd6f29517a7d3d24c4c23dfc1837cc
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-11 14:28:31 +0100

    Actualizar personalize_btn.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
@@ -1,27 +1,26 @@
-/** @odoo-module **/
-import publicWidget from 'web.public.widget';
+/** serial_printer_custom_wizard/static/src/js/personalize_btn.js */
+odoo.define('serial_printer_custom_wizard.personalize_btn', function (require) {
+  "use strict";
+  const publicWidget = require('web.public.widget');
 
-publicWidget.registry.SpwPersonalizeBtn = publicWidget.Widget.extend({
-    selector: 'form.o_wsale_product_form',
+  publicWidget.registry.SPWPlacePersonalize = publicWidget.Widget.extend({
+    selector: 'body',
     start() {
-        this._updateHref();
-        // Odoo actualiza este hidden con la variante seleccionada
-        this.$el.on('change', 'input[name="product_id"]', this._updateHref.bind(this));
-        // Por si el tema emite cambios en otros inputs de atributos
-        this.$el.on('change', 'input.js_variant_change, select.js_variant_change', this._updateHref.bind(this));
-        return this._super(...arguments);
-    },
-    _updateHref() {
-        const variantId = this.$el.find('input[name="product_id"]').val();
-        const $btn = this.$el.find('#spw_personalize_btn');
-        if (!$btn.length || !variantId) return;
+      const ctn = document.getElementById('spw_personalize_ctn');
+      if (!ctn) return Promise.resolve();
 
-        // Base = /personalizar/<product_template_id>
-        let base = $btn.data('base');
-        if (!base) {
-            base = $btn.attr('href');
-            $btn.attr('data-base', base);
-        }
-        $btn.attr('href', `${base}?variant_id=${encodeURIComponent(variantId)}`);
+      // Posibles contenedores de botones según tema/versión
+      const targets = [
+        '.o_wsale_product_form .o_wsale_product_btns',
+        '.o_wsale_product_form .o_wsale_product_btn',
+        '.o_wsale_product_form',
+        '#product_details',
+      ];
+      for (const sel of targets) {
+        const host = document.querySelector(sel);
+        if (host) { host.appendChild(ctn); break; }
+      }
+      return Promise.resolve();
     },
+  });
 });
