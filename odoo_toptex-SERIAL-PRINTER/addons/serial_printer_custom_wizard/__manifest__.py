a7d4720983487d2d4d2bbc77a65f4c5d0dce179e
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-11 11:40:01 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,29 +1,17 @@
-# addons/serial_printer_custom_wizard/__manifest__.py
+# -*- coding: utf-8 -*-
 {
     "name": "Serial Printer – Custom Wizard",
-    "summary": "Personalización de productos en eCommerce",
-    "version": "18.0.1.0.0",
-    "category": "Website/Commerce",
-    "author": "Serial Printer",
-    "license": "LGPL-3",
-    "depends": ["website", "website_sale"],
+    "version": "16.0.1.0.0",
+    "depends": ["website_sale"],
     "data": [
+        "views/product_page_button.xml",
         "views/customizer_page.xml",
-        "views/personalizacion_form.xml",
-        "views/personalizacion_wizard_views.xml",
-        "views/personalizacion_wizard_website_views.xml",
     ],
     "assets": {
         "web.assets_frontend": [
-            "serial_printer_custom_wizard/static/src/js/add_customize_button.js",
             "serial_printer_custom_wizard/static/src/js/customizer_preview.js",
             "serial_printer_custom_wizard/static/src/css/spw.css",
-            "serial_printer_custom_wizard/static/src/js/customizer_enhancements.js",
-            "serial_printer_custom_wizard/static/src/css/spw_colors.css",
-            # --- SOLO ESTA LÍNEA NUEVA ---
-            "serial_printer_custom_wizard/static/src/js/spw_force_customize_button.js",
         ],
     },
-    "installable": True,
-    "application": False,
+    "license": "LGPL-3",
 }
