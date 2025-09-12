8c34867a714a66b446a9b64a33cdc3fce5c597e6
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-12 09:36:39 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 {
     "name": "Serial Printer — Custom Wizard",
     "summary": "Personalización de productos en eCommerce",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",  # <-- SUBIDA DE VERSIÓN PARA RECARGAR PYTHON
     "category": "Website/Commerce",
     "author": "Serial Printer",
     "license": "LGPL-3",
@@ -10,7 +11,7 @@
         "views/customizer_page.xml",
         "views/personalizacion_form.xml",
         "views/personalizacion_wizard_views.xml",
-        "views/product_personalize_button.xml",  # botón en la ficha
+        "views/product_personalize_button.xml",
     ],
     "assets": {
         "web.assets_frontend": [
@@ -18,7 +19,7 @@
             "serial_printer_custom_wizard/static/src/js/move_personalize_btn.js",
             "serial_printer_custom_wizard/static/src/js/customizer_preview.js",
             "serial_printer_custom_wizard/static/src/js/personalize_btn.js",
-            "serial_printer_custom_wizard/static/src/js/customizer.js",  # requerido por tu petición
+            "serial_printer_custom_wizard/static/src/js/customizer.js",
         ],
     },
     "installable": True,
