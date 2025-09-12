ac11227ee95bf7804210f35ce2634e293f605509
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-12 10:36:38 +0100

    Actualizar main.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/main.py
@@ -2,17 +2,24 @@
 from odoo import http
 from odoo.http import request
 
-class SPWController(http.Controller):
+class SpwCustomizer(http.Controller):
 
-    # Acepta /spw/customize y /spw/customize/<tmpl_id>
-    @http.route(['/spw/customize', '/spw/customize/<int:tmpl_id>'],
+    @http.route(['/spw/customize/<int:product_tmpl_id>'],
                 type='http', auth='public', website=True, sitemap=False)
-    def spw_customize(self, tmpl_id=None, **kw):
-        values = {}
-        # Si nos pasan el template, lo mandamos a la vista (no hace falta, pero permite usar product si quieres)
-        if tmpl_id:
-            product = request.env['product.template'].sudo().browse(tmpl_id)
-            if product.exists():
-                values['product'] = product
-        # Renderiza SIEMPRE la página (no 404)
+    def spw_customize(self, product_tmpl_id, variant_id=None, **kw):
+        # template
+        tmpl = request.env['product.template'].sudo().browse(product_tmpl_id).exists()
+        if not tmpl:
+            return request.not_found()
+
+        variant = None
+        if variant_id:
+            variant = request.env['product.product'].sudo().browse(int(variant_id)).exists()
+            if variant and variant.product_tmpl_id.id != tmpl.id:
+                tmpl = variant.product_tmpl_id
+
+        values = {
+            'product': tmpl,   # tu template customizer usa 'product'
+            'variant': variant,
+        }
         return request.render('serial_printer_custom_wizard.spw_customize_page', values)
