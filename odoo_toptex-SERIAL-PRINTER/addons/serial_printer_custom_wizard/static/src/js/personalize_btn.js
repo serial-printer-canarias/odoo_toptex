2136d2a41deb25f6fcedb84a27d6a2fa816994df
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-12 10:35:01 +0100

    Actualizar personalize_btn.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
@@ -1,40 +1,32 @@
-/** Abre el personalizador con los parámetros correctos */
+/** @odoo-module */
 odoo.define('serial_printer_custom_wizard.personalize_btn', function (require) {
-    'use strict';
+  'use strict';
 
-    const publicWidget = require('web.public.widget');
+  const publicWidget = require('web.public.widget');
 
-    publicWidget.registry.SPWPersonalizeButton = publicWidget.Widget.extend({
-        selector: '#spw_personalize_btn',
-        events: { 'click': '_onClick' },
+  publicWidget.registry.SpwPersonalizeBtn = publicWidget.Widget.extend({
+    selector: '#spw_personalize_btn',
 
-        _onClick: function (ev) {
-            ev.preventDefault();
+    start() {
+      this._buildUrl();
+      this.$el.on('click', (ev) => {
+        ev.preventDefault();
+        this._buildUrl();
+        const href = this.$el.attr('href');
+        if (href) {
+          window.location.href = href;
+        }
+      });
+      return this._super(...arguments);
+    },
 
-            const $form = $('form.o_wsale_product_form, form#product_form');
-
-            // Variante seleccionada
-            const variantId = ($form.find('input.product_id:checked').val()
-                            || $form.find('input[name="product_id"]').val()
-                            || null);
-
-            // Template por si lo necesitas
-            const tmplId = ($form.find('input[name="product_template_id"]').val()
-                         || $form.data('product-template-id')
-                         || null);
-
-            // Imagen actual en la ficha
-            const img = document.querySelector('.o_wsale_product_img img, img.js_product_img, img.product_detail_img');
-            const imgUrl = img ? img.getAttribute('src') : null;
-
-            const params = new URLSearchParams();
-            if (variantId) params.set('variant_id', variantId);
-            if (tmplId) params.set('tmpl_id', tmplId);
-            if (imgUrl) params.set('image', imgUrl);
-
-            // IMPORTANTE: SIEMPRE a /spw/customize (sin ID al final)
-            const url = '/spw/customize' + (params.toString() ? ('?' + params.toString()) : '');
-            window.location.href = url;
-        },
-    });
+    _buildUrl() {
+      const slot = document.getElementById('spw_personalize_slot');
+      const tmplId = slot && slot.dataset.ptmpl ? slot.dataset.ptmpl : '';
+      const variantInput = document.querySelector("input[name='product_id']");
+      const variantId = variantInput ? variantInput.value : '';
+      const url = `/spw/customize/${tmplId}${variantId ? `?variant_id=${variantId}` : ''}`;
+      this.$el.attr('href', url);
+    },
+  });
 });
