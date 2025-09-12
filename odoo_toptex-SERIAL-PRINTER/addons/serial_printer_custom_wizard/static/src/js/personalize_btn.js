66b6eae594443bac49e14cc28c1cd2f1d3b5259d
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-12 18:51:14 +0100

    Actualizar personalize_btn.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/personalize_btn.js
@@ -1,13 +1,11 @@
-// JS plano para evitar errores del module loader
 document.addEventListener("click", function (ev) {
   const btn = ev.target.closest("#spw_personalize_btn");
   if (!btn) return;
-
   ev.preventDefault();
-  const tmplId = btn.dataset.productTemplateId;
-  const productIdInput = document.querySelector("form input[name='product_id']");
-  const variantId = productIdInput && productIdInput.value;
 
+  const tmplId = btn.dataset.productTemplateId;
+  const inputVariant = document.querySelector("form input[name='product_id']");
+  const variantId = inputVariant && inputVariant.value;
   if (tmplId && variantId) {
     window.location.href = `/spw/customize/${tmplId}?variant_id=${variantId}`;
   }
