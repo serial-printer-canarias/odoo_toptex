a471e9bff049f8f3cc3d8e8a231b95e3082f1741
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-15 17:42:09 +0100

    Actualizar product_matrix.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/product_matrix.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/product_matrix.js
@@ -1,9 +1,5 @@
-/** serial_printer_web_custom - product_matrix.js
- *  Añade al carrito todas las tallas con cantidad > 0 del bloque del color pulsado.
- */
 odoo.define('serial_printer_web_custom.product_matrix', function (require) {
     'use strict';
-
     const publicWidget = require('web.public.widget');
     const ajax = require('web.ajax');
 
@@ -12,43 +8,36 @@ odoo.define('serial_printer_web_custom.product_matrix', function (require) {
         events: {
             'click .add-to-cart-btn': '_onAddToCartColor',
         },
-
         _onAddToCartColor: function (ev) {
             ev.preventDefault();
             const $btn = $(ev.currentTarget);
             const $card = $btn.closest('.border.rounded.p-3');
             const $inputs = $card.find('input.qty-input');
             const calls = [];
-
             $inputs.each(function () {
                 const $inp = $(this);
                 const qty = parseFloat($inp.val());
                 const variantId = parseInt($inp.data('variant-id'), 10);
                 if (qty > 0 && variantId) {
-                    calls.push(
-                        ajax.jsonRpc('/shop/cart/update_json', 'call', {
-                            product_id: variantId,
-                            add_qty: qty,
-                            display: false,
-                        })
-                    );
+                    calls.push(ajax.jsonRpc('/shop/cart/update_json', 'call', {
+                        product_id: variantId,
+                        add_qty: qty,
+                        display: false,
+                    }));
                 }
             });
-
             if (!calls.length) {
                 $btn.addClass('btn-outline-secondary');
                 setTimeout(() => $btn.removeClass('btn-outline-secondary'), 800);
                 return;
             }
-
             Promise.all(calls).then(() => {
-                window.location.reload(); // refresca cantidades y mini-carrito
+                window.location.reload();
             }).catch(() => {
                 $btn.removeClass('btn-primary').addClass('btn-danger');
                 setTimeout(() => $btn.removeClass('btn-danger').addClass('btn-primary'), 2000);
             });
         },
     });
-
     return publicWidget.registry.SerialPrinterMatrix;
 });
