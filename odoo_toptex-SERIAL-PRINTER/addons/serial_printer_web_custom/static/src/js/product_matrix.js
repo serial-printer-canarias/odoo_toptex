7b9cf4d57515a084a01641a8bcd049c4ef7f471c
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-15 19:58:03 +0100

    Actualizar product_matrix.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/product_matrix.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/product_matrix.js
@@ -6,16 +6,15 @@ odoo.define('serial_printer_web_custom.product_matrix', function (require) {
     publicWidget.registry.SerialPrinterMatrix = publicWidget.Widget.extend({
         selector: '.o_wsale_product_page',
         events: { 'click .add-to-cart-btn': '_onAddToCartColor' },
+
         _onAddToCartColor: function (ev) {
             ev.preventDefault();
             const $btn = $(ev.currentTarget);
             const $card = $btn.closest('.border.rounded.p-3');
-            const $inputs = $card.find('input.qty-input');
             const calls = [];
-            $inputs.each(function () {
-                const $inp = $(this);
-                const qty = parseFloat($inp.val());
-                const product_id = parseInt($inp.data('variant-id'), 10);
+            $card.find('input.qty-input').each(function () {
+                const qty = parseFloat(this.value || '0');
+                const product_id = parseInt(this.dataset.variantId || '0', 10);
                 if (qty > 0 && product_id) {
                     calls.push(ajax.jsonRpc('/shop/cart/update_json', 'call', {
                         product_id, add_qty: qty, display: false,
