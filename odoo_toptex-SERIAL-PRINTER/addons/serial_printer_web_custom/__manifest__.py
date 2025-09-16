b00af121cd1bd06425dabb905ec0895019294ec0
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-16 14:03:43 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,20 +1,19 @@
+# addons/serial_printer_web_custom/__manifest__.py
 {
-    "name": "Serial Printer Web Custom",
-    "version": "18.0.1.0.0",
-    "category": "Website",
-    "summary": "Matriz de tallas en ficha de producto del eCommerce",
-    "author": "Serial Printer",
-    "license": "LGPL-3",
-    "depends": ["website_sale"],
-    "data": [],
-    "assets": {
-        # Cargamos en el frontend de Website
-        "website.assets_frontend": [
-            "serial_printer_web_custom/static/src/js/product_matrix.js",
-            "serial_printer_web_custom/static/src/css/product_matrix.css",
+    'name': 'Serial Printer Web Custom',
+    'version': '18.0.1.0.0',
+    'category': 'Website/Website',
+    'summary': 'Grid de cantidades por color/talla en la ficha de producto',
+    'license': 'LGPL-3',
+    'depends': ['website_sale', 'website_sale_stock'],
+    'data': [],
+    'assets': {
+        'web.assets_frontend': [
+            'serial_printer_web_custom/static/src/js/product_matrix.js',
+            'serial_printer_web_custom/static/src/scss/product_matrix.scss',
         ],
     },
-    "installable": True,
-    "application": False,
-    "auto_install": False,
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
