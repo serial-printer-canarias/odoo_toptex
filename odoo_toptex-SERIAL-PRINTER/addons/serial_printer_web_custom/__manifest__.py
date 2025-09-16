7bd565b1ac6ed2a886fef35aff6e986f13215c14
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-16 12:57:11 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,21 +1,16 @@
 {
-    "name": "Serial Printer Web Custom",
-    "version": "18.0.1.0.0",
-    "category": "Website",
-    "summary": "Ajustes de la vista de producto (banner/matriz)",
-    "author": "Serial Printer",
-    "license": "LGPL-3",
-    "depends": ["website_sale"],
-    "data": [
-        "views/product_template.xml",
-    ],
-    "assets": {
-        "web.assets_frontend": [
-            # Si más adelante quieres JS propio, deja esta línea
-            "serial_printer_web_custom/static/src/js/product_matrix.js",
+    'name': 'Serial Printer Web Custom',
+    'version': '18.0.1.0.0',
+    'category': 'Website',
+    'depends': ['website_sale'],
+    'assets': {
+        'web.assets_frontend': [
+            'serial_printer_web_custom/static/src/js/product_matrix.js',
+            # opcional si tienes estilos:
+            # 'serial_printer_web_custom/static/src/scss/product_matrix.scss',
         ],
     },
-    "installable": True,
-    "application": False,
-    "auto_install": False,
+    'installable': True,
+    'application': False,
+    'auto_install': False,
 }
