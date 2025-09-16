96fe534b6af2f1f70008ac499a7a1f95efa4b7b8
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-16 13:17:04 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,16 +1,20 @@
 {
-    'name': 'Serial Printer Web Custom',
-    'version': '18.0.1.0.0',
-    'category': 'Website',
-    'depends': ['website_sale'],
-    'assets': {
-        'web.assets_frontend': [
-            'serial_printer_web_custom/static/src/js/product_matrix.js',
-            # opcional si tienes estilos:
-            # 'serial_printer_web_custom/static/src/scss/product_matrix.scss',
+    "name": "Serial Printer Web Custom",
+    "version": "18.0.1.0.0",
+    "category": "Website",
+    "summary": "Matriz de tallas en ficha de producto del eCommerce",
+    "author": "Serial Printer",
+    "license": "LGPL-3",
+    "depends": ["website_sale"],
+    "data": [],
+    "assets": {
+        # Cargamos en el frontend de Website
+        "website.assets_frontend": [
+            "serial_printer_web_custom/static/src/js/product_matrix.js",
+            "serial_printer_web_custom/static/src/css/product_matrix.css",
         ],
     },
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
