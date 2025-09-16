114d18bc61694c79a90eb83ad4e7276a78eb3936
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-16 08:11:34 +0100

    Actualizar sale_order_line.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_order_line.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_order_line.py
@@ -4,10 +4,6 @@ from odoo import models, fields
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    x_spw_technique = fields.Char('Técnica')
-    x_spw_color_code = fields.Char('Código color')
-    x_spw_color_hex = fields.Char('Color HEX')
-    x_spw_positions = fields.Char('Posiciones')  # pecho_izq,pecho_dcha,espalda,libre
-    x_spw_notes = fields.Text('Observaciones')
-    x_spw_logo_attachment_id = fields.Many2one('ir.attachment', string='Logo adjunto')
-    x_spw_params_json = fields.Text('Parámetros de previsualización')
+    x_personalization_json = fields.Text('Personalización (JSON)')
+    x_personalization_png = fields.Binary('Mockup PNG')
+    x_personalization_filename = fields.Char('Nombre PNG')
