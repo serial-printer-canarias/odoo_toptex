4612a45ec52ee193458fb3e4bcd988391d4a00c1
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-17 08:21:08 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
 {
-    'name': 'Serial Printer Web Custom',
-    'version': '18.0.1.0.0',
-    'depends': ['website_sale'],
-    'data': [],
-    'assets': {
-        'web.assets_frontend': [
-            # OJO: solo este archivo. Nada de SCSS por ahora.
-            'serial_printer_web_custom/static/src/js/sp_matrix_boot.js',
+    "name": "Serial Printer Web Custom",
+    "summary": "Rejilla Color x Talla en la ficha de producto (Website)",
+    "version": "18.0.1.0.0",
+    "category": "Website/Shop",
+    "license": "LGPL-3",
+    "author": "Serial Printer",
+    "depends": ["website_sale"],
+    "data": [],  # << NO cargamos vistas para evitar 500; todo va por assets
+    "assets": {
+        # Frontend (tienda). Si prefieres, puedes dejar solo uno de los dos bundles.
+        "web.assets_frontend": [
+            "serial_printer_web_custom/static/src/js/product_matrix.js",
+            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
+        ],
+        "website.assets_frontend": [
+            "serial_printer_web_custom/static/src/js/product_matrix.js",
+            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
+    "installable": True,
+    "application": False,
 }
