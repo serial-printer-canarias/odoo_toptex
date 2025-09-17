759b7bc09e16b8f34cfcc06e7c15e37544d740c3
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-17 10:01:37 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,20 +1,10 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Serial Printer Web Custom",
-    "summary": "Rejilla Color x Talla en la ficha de producto (Website)",
-    "version": "18.0.1.0.0",
-    "category": "Website/Shop",
-    "license": "LGPL-3",
-    "author": "Serial Printer",
+    "version": "18.0.0.7",
+    "category": "Website",
     "depends": ["website_sale"],
-    "data": [],  # << NO cargamos vistas para evitar 500; todo va por assets
     "assets": {
-        # Frontend (tienda). Si prefieres, puedes dejar solo uno de los dos bundles.
         "web.assets_frontend": [
-            "serial_printer_web_custom/static/src/js/product_matrix.js",
-            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
-        ],
-        "website.assets_frontend": [
             "serial_printer_web_custom/static/src/js/product_matrix.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
