28a5d696dc9e578d32be08fd7d7ce0405c15e7bc
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-22 10:43:35 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,19 +1,22 @@
-# -*- coding: utf-8 -*-
 {
     "name": "Serial Printer Web Custom",
     "version": "18.0.1.0.0",
     "category": "Website",
-    "summary": "Product matrix (color x talla) con imagen, stock, precio y añadir múltiple al carrito",
-    "depends": ["website_sale", "stock"],
+    "summary": "Product matrix (color x talla) con precio/stock",
+    "depends": ["website_sale"],
     "data": [
-        "views/sp_matrix_anchor.xml",
+        "views/sp_matrix_anchor.xml",   # <-- solo el ancla, nada más
     ],
     "assets": {
+        # Lo cargamos en ambos bundles para evitar sorpresas del tema
         "web.assets_frontend": [
+            "serial_printer_web_custom/static/src/js/product_matrix.js",
+            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
+        ],
+        "website.assets_wsale": [
             "serial_printer_web_custom/static/src/js/product_matrix.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
     "license": "LGPL-3",
-    "installable": True,
 }
