44037743f5e92cead2657f4f64352546e516b4e5
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-22 07:46:35 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,19 +1,15 @@
 {
     "name": "Serial Printer Web Custom",
-    "version": "18.0.1.0.2",
+    "version": "18.0.1.0.0",
     "category": "Website",
     "summary": "Product matrix (color x talla) con precio/stock",
-    "license": "LGPL-3",
-    "depends": ["website_sale"],
+    "depends": ["website_sale", "stock"],
     "data": [],
     "assets": {
-        "web.assets_frontend_minimal": [
-            "serial_printer_web_custom/static/src/js/product_matrix.js",
-            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
-        ],
         "web.assets_frontend": [
             "serial_printer_web_custom/static/src/js/product_matrix.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
+    "license": "LGPL-3",
 }
