27e84b42cbb98f61cdefb812cb95878c689aeec0
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-22 12:47:50 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,16 +1,19 @@
+# __manifest__.py  (Odoo 18)
 {
     "name": "Serial Printer - Web Custom (Matrix)",
-    "version": "1.0",
+    "version": "18.0.1.0.0",
+    "license": "LGPL-3",
     "category": "Website",
-    "summary": "Grid de cantidades por color/talla en la ficha",
+    "summary": "Grid de cantidades por color/talla en la ficha de producto",
     "depends": ["website_sale"],
+    "data": [],  # ¡SIN XML para evitar el error de xpath!
     "assets": {
-        "web.assets_frontend": [
+        # Bundle específico de eCommerce
+        "website.assets_wsale": [
             "serial_printer_web_custom/static/src/js/product_matrix.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
-    "data": [],
     "installable": True,
     "application": False,
 }
