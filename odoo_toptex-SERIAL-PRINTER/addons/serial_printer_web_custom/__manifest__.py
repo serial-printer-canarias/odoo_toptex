7457b25b96c255ddd83323f1d2d107518500722f
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-23 19:16:57 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Serial Printer – Web Custom (Matrix)",
-    "version": "18.0.1.0",
-    "summary": "Matriz por color/talla con precio, stock, foto y añadir al carrito en lote",
+    "name": "Serial Printer | Web Custom (Matrix)",
+    "version": "18.0.1.0.0",
+    "category": "Website/Website",
+    "summary": "Product matrix (color/size) on product page",
     "author": "Serial Printer",
-    "website": "https://serial-printer.example",
+    "website": "https://example.com",
     "license": "LGPL-3",
     "depends": ["website_sale"],
     "data": [
@@ -12,12 +13,11 @@
     ],
     "assets": {
         "web.assets_frontend": [
-            # Debug seguro para que no vuelva a salir "_sp is not defined"
             "serial_printer_web_custom/static/src/js/sp_probe.esm.js",
-            # Matriz
             "serial_printer_web_custom/static/src/js/product_matrix.esm.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
     "installable": True,
+    "application": False,
 }
