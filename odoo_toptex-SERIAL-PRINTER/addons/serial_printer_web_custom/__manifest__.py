90694450b6a8bd8bf4bd81444e63d0fd751f342e
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-23 18:52:27 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/__manifest__.py
@@ -1,23 +1,23 @@
+# -*- coding: utf-8 -*-
 {
-    "name": "Serial Printer Web Custom",
-    "version": "18.0.1.0.0",
-    "summary": "Matriz de variantes en la ficha de producto",
-    "category": "Website/Website",
+    "name": "Serial Printer – Web Custom (Matrix)",
+    "version": "18.0.1.0",
+    "summary": "Matriz por color/talla con precio, stock, foto y añadir al carrito en lote",
+    "author": "Serial Printer",
+    "website": "https://serial-printer.example",
     "license": "LGPL-3",
     "depends": ["website_sale"],
-    "data": [],  # no XML hasta confirmar que el asset carga
+    "data": [
+        "views/sp_matrix_anchor.xml",
+    ],
     "assets": {
-        # Odoo 18 usa este bundle en la web
         "web.assets_frontend": [
+            # Debug seguro para que no vuelva a salir "_sp is not defined"
             "serial_printer_web_custom/static/src/js/sp_probe.esm.js",
-            "serial_printer_web_custom/static/src/js/product_matrix.esm.js",
-            "serial_printer_web_custom/static/src/scss/product_matrix.scss",
-        ],
-        # Por si tu tema referenciara el bundle de website (no debería, pero no molesta)
-        "website.assets_frontend": [
-            "serial_printer_web_custom/static/src/js/sp_probe.esm.js",
+            # Matriz
             "serial_printer_web_custom/static/src/js/product_matrix.esm.js",
             "serial_printer_web_custom/static/src/scss/product_matrix.scss",
         ],
     },
+    "installable": True,
 }
