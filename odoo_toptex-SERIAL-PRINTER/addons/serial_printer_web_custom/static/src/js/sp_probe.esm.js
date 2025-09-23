4a648ad73ec2134ede80c30f86a9e85e06d04916
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-23 19:46:25 +0100

    Actualizar sp_probe.esm.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/sp_probe.esm.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_web_custom/static/src/js/sp_probe.esm.js
@@ -1,9 +1,8 @@
-// Sonda simple para comprobar carga de assets y evitar ReferenceError
+// Sonda de carga de assets (segura)
 window._sp = window._sp || {};
 _sp.debug = _sp.debug || {
-  log: (...a) => console.log('[SP]', ...a),
-  ok:  (msg) => console.log('%c[SP] ' + msg, 'color:#0a0'),
-  warn:(msg) => console.warn('[SP]', msg),
+  log:  (...a) => console.log('[SP]', ...a),
+  ok:   (m) => console.log('%c[SP] ' + m, 'color:#0a0'),
+  warn: (m) => console.warn('[SP]', m),
 };
-
 _sp.debug.ok('web.assets_frontend cargado ✅');
