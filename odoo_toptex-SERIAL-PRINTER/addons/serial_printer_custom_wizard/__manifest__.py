c55973700f3823318fdc629cdf137629d26008fd
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-24 09:49:47 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -9,12 +9,11 @@
     "data": [
         "views/customizer_page.xml",
         "views/product_personalize_button.xml",
-        # ⚠️ QUITA el XML que daba error de XPath:
-        # "views/spw_cart_preview_qweb.xml",
-        # ⚠️ Y no cargues ya ningún “*_inject.xml”
+        # NO tocamos más QWeb para el carrito
     ],
     "assets": {
         "web.assets_frontend": [
+            # --- lo que ya tenías ---
             "serial_printer_custom_wizard/static/src/js/spw_button.js",
             "serial_printer_custom_wizard/static/src/js/spw_logo_preview.js",
             "serial_printer_custom_wizard/static/src/js/customizer.js",
@@ -22,8 +21,9 @@
             "serial_printer_custom_wizard/static/src/js/spw_options.js",
             "serial_printer_custom_wizard/static/src/js/spw_cart.js",
             "serial_printer_custom_wizard/static/src/js/spw.js",
-            "serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js",  # ⬅️ NUEVO
             "serial_printer_custom_wizard/static/src/css/spw.css",
+            # --- AÑADIR ESTE ARCHIVO NUEVO (el que inyecta en el carrito) ---
+            "serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js",
         ],
     },
     "installable": True,
