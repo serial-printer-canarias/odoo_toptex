8f5ec45c4d41b8e544c1ea8be9500007e86b1bc1
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 11:39:53 +0100

    Actualizar spw_cart_token.js

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/spw_cart_token.js
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/static/src/js/spw_cart_token.js
@@ -1,33 +1,29 @@
 /** SPW – Forzar líneas independientes en carrito (token por personalización) */
-odoo.define('serial_printer_custom_wizard.spw_cart_token', function (require) {
+odoo.define('serial_printer_custom_wizard/js/spw_cart_token', function (require) {
     'use strict';
 
     var ajax = require('web.ajax');
 
     function uid() {
-        // token corto pero único suficiente para separar líneas
-        return 'spw_' + Math.random().toString(36).slice(2, 10) + Date.now().toString(36).slice(-6);
+        return 'spw_' + Math.random().toString(36).slice(2, 10) + '_' + Date.now().toString(36);
     }
 
-    // Detecta si estamos en contexto del wizard (ajusta el selector si usas otro)
+    // Detección del contexto del wizard (más tolerante)
     function inWizardContext() {
-        return !!document.querySelector('.spw-wizard, [data-spw-wizard="1"]');
+        if (document.querySelector('.spw-wizard,[data-spw-wizard="1"],form[action*="/spw/"]')) return true;
+        var href = window.location.href;
+        return /\/spw\/|[\?&]spw=1/.test(href);
     }
 
-    // Parchea jsonRpc para añadir spw_token cuando se llama a /shop/cart/update_json
+    // Parcheamos jsonRpc sólo para /shop/cart/update_json cuando estamos en el wizard
     var _jsonRpc = ajax.jsonRpc;
     ajax.jsonRpc = function (url, method, params) {
         try {
             if (url && url.indexOf('/shop/cart/update_json') !== -1 && inWizardContext()) {
                 params = params || {};
-                // Sólo si no viene ya uno del backend
-                if (!params.spw_token) {
-                    params.spw_token = uid();
-                }
-                // IMPORTANTÍSIMO: NO pasar line_id para no actualizar una línea existente
-                if ('line_id' in params) {
-                    delete params.line_id;
-                }
+                if (!params.spw_token) params.spw_token = uid();
+                // clave: no actualizar una línea existente ⇒ obligamos a crear línea nueva
+                if ('line_id' in params) delete params.line_id;
             }
         } catch (e) {
             console.warn('[SPW] token inject warn:', e);
@@ -35,5 +31,5 @@ odoo.define('serial_printer_custom_wizard.spw_cart_token', function (require) {
         return _jsonRpc.call(this, url, method, params);
     };
 
-    console.log('[SPW] cart_token listo (líneas separadas por spw_token)');
+    console.log('[SPW] cart_token activo (líneas separadas por spw_token)');
 });
