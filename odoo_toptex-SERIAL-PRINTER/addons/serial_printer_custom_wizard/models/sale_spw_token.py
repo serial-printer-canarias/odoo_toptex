517c88e9a5b0d4a8b689edceaf83902104296b5d
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 11:23:30 +0100

    Actualizar sale_spw_token.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_token.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_token.py
@@ -4,5 +4,4 @@ from odoo import models, fields
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # Un identificador por personalización (no se copia ni se rellena solo)
-    spw_token = fields.Char(index=True, copy=False)
+    spw_token = fields.Char(index=True, copy=False, help="Identificador único por personalización para no fusionar líneas.")
