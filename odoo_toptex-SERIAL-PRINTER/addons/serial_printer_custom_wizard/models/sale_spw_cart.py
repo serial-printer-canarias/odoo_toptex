57d0b23405e93b1cffbacb00eee4f9e748ab8886
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 14:24:29 +0100

    Actualizar sale_spw_cart.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
@@ -5,19 +5,18 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _cart_find_product_line(self, product_id=None, line_id=None, **kwargs):
-        lines = super()._cart_find_product_line(product_id=product_id, line_id=line_id, **kwargs)
+        """Si recibimos spw_token, NO buscamos línea: forzamos crear una nueva.
+        Sin token: nunca casar con líneas que ya tengan token (separar 'sin pers' vs 'con pers')."""
         token = kwargs.get('spw_token')
         if token:
-            # Sólo casar con la línea que tenga el mismo token
-            return lines.filtered(lambda l: l.spw_token == token)
-        # Sin token, nunca mezclar con líneas personalizadas
+            return self.env['sale.order.line']  # <- fuerza 'no match' => crea línea nueva
+        lines = super()._cart_find_product_line(product_id=product_id, line_id=line_id, **kwargs)
         return lines.filtered(lambda l: not l.spw_token)
 
     def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0, **kwargs):
         token = kwargs.get('spw_token')
-        # Si hay token, forzamos crear línea nueva
         if token:
-            line_id = None
+            line_id = None  # <- importantísimo: jamás actualizar línea existente
         res = super()._cart_update(product_id=product_id, line_id=line_id, add_qty=add_qty, set_qty=set_qty, **kwargs)
         if token and res.get('line_id'):
             self.env['sale.order.line'].sudo().browse(res['line_id']).write({'spw_token': token})
