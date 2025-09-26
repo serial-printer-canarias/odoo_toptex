0d19bcdab75a9604b7888363eb1d6b38c7e63e72
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 16:11:58 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -22,7 +22,6 @@
             "serial_printer_custom_wizard/static/src/js/spw_cart.js",
             "serial_printer_custom_wizard/static/src/js/spw.js",
             "serial_printer_custom_wizard/static/src/css/spw.css",
-            "serial_printer_custom_wizard/static/src/js/spw_cart_token.js",
             # --- AÑADIR ESTE ARCHIVO NUEVO (el que inyecta en el carrito) ---
             "serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js",
         ],
