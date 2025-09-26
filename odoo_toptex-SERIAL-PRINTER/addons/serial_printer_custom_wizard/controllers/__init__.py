6df0dd84d93e69f9d19c72df630755ed6f6cc312
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 10:56:51 +0100

    Actualizar __init__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/__init__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/__init__.py
@@ -4,3 +4,4 @@ from . import main            # tu archivo original (no lo toco)
 from . import spw_add_to_cart # <-- IMPORTANTE: registra la ruta nueva
 from . import customizer
 from . import controllers
+from . import spw_preview
