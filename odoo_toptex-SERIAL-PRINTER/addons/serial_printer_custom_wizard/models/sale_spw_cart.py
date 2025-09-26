8759e3a6e89862efb54e4a0c932347bc8f8824c9
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-26 10:45:26 +0100

    Actualizar sale_spw_cart.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+import uuid
+from odoo import http
+from odoo.http import request
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+class SPWCartController(http.Controller):
 
-    def _cart_find_product_line(self, product_id=None, line_id=None, **kwargs):
-        """
-        Si recibimos spw_token, sólo permitimos casar con una línea que tenga
-        el MISMO token. Si no coincide, devolvemos vacío para que cree una nueva.
-        """
-        res = super()._cart_find_product_line(product_id=product_id, line_id=line_id, **kwargs)
-        token = kwargs.get('spw_token')
-        if token:
-            res = res.filtered(lambda l: l.spw_token == token)
+    @http.route(['/spw/add_to_cart'], type='json', auth='public', website=True, csrf=False)
+    def spw_add_to_cart(self, product_id, quantity=1, **kw):
+        # token único por personalización
+        spw_token = kw.get('spw_token') or uuid.uuid4().hex[:12]
+
+        order = request.website.sale_get_order(force_create=True)
+        res = order._cart_update(
+            product_id=int(product_id),
+            add_qty=float(quantity),
+            # ¡sin line_id para no forzar update de una línea existente!
+            spw_token=spw_token,
+        )
+
+        # Respondemos con el token y la line_id creada (útil para previews)
+        res['spw_token'] = spw_token
         return res
-
-    def _cart_update(self, product_id=None, line_id=None, add_qty=0, set_qty=0, **kwargs):
-        """
-        Tras crear/actualizar la línea, si hay spw_token lo fijamos en la línea.
-        """
-        token = kwargs.get('spw_token')
-        out = super()._cart_update(product_id=product_id, line_id=line_id, add_qty=add_qty, set_qty=set_qty, **kwargs)
-        if token and out.get('line_id'):
-            self.env['sale.order.line'].sudo().browse(out['line_id']).write({'spw_token': token})
-        return out
