403ad51749b425620e23441ec42a17d20d9c813f
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-27 21:49:11 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,39 +1,39 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Serial Printer - Custom Wizard",
-    "version": "18.0.1.2.0",   # <- súbela en cada update
+    "version": "18.0.1.3.0",   # súbela en cada commit
     "summary": "Personalización con preview y múltiples imágenes por línea.",
     "category": "Website/Website",
     "license": "LGPL-3",
     "depends": ["website_sale"],
 
-    # SOLO las vistas que existen en /views
     "data": [
         "views/customizer_page.xml",
         "views/product_personalize_button.xml",
     ],
 
-    # TODOS los CSS/JS que tienes en /static/src/{css,js}
     "assets": {
         "web.assets_frontend": [
-            # CSS
-            "serial_printer_custom_wizard/static/src/css/customizer.css",
-            "serial_printer_custom_wizard/static/src/css/personalizar_preview.css",
-            "serial_printer_custom_wizard/static/src/css/spw.css",
-            "serial_printer_custom_wizard/static/src/css/spw_colors.css",
+            # ===== CSS (OJO: carpeta 'CSS' en mayúsculas) =====
+            "serial_printer_custom_wizard/static/src/CSS/customizer.css",
+            "serial_printer_custom_wizard/static/src/CSS/personalizar_preview.css",
+            "serial_printer_custom_wizard/static/src/CSS/spw.css",
+            "serial_printer_custom_wizard/static/src/CSS/spw_colors.css",
 
-            # JS (ordenado: utilidades -> botón -> customizer -> carrito -> inyectores)
+            # ===== JS utilidades / base =====
             "serial_printer_custom_wizard/static/src/js/spw_public.js",
             "serial_printer_custom_wizard/static/src/js/spw.js",
             "serial_printer_custom_wizard/static/src/js/spw_options.js",
             "serial_printer_custom_wizard/static/src/js/ns300_colors.js",
 
+            # ===== Botón / colocación =====
             "serial_printer_custom_wizard/static/src/js/spw_button.js",
             "serial_printer_custom_wizard/static/src/js/personalize_btn.js",
             "serial_printer_custom_wizard/static/src/js/add_customize_button.js",
             "serial_printer_custom_wizard/static/src/js/move_personalize_btn.js",
             "serial_printer_custom_wizard/static/src/js/spw_force_customize_button.js",
 
+            # ===== Customizer / previews =====
             "serial_printer_custom_wizard/static/src/js/spw_customizer.js",
             "serial_printer_custom_wizard/static/src/js/customizer.js",
             "serial_printer_custom_wizard/static/src/js/customizer_enhancements.js",
@@ -41,6 +41,7 @@
             "serial_printer_custom_wizard/static/src/js/spw_logo_preview.js",
             "serial_printer_custom_wizard/static/src/js/personalizacion.js",
 
+            # ===== Carrito =====
             "serial_printer_custom_wizard/static/src/js/spw_cart.js",
             "serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js",
         ],
