04deb3f8b978d17d75c742538f815440857bc970
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-27 22:20:30 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -1,31 +1,32 @@
-# -*- coding: utf-8 -*-
 {
-    "name": "Serial Printer - Custom Wizard",
-    "version": "18.0.1.0.0",
-    "summary": "Personalización de producto con vista previa de logo.",
-    "category": "Website/Website",
-    "license": "LGPL-3",
-    "depends": ["website_sale"],
-    "data": [
-        "views/customizer_page.xml",
-        "views/product_personalize_button.xml",
-        # NO tocamos más QWeb para el carrito
+    'name': 'Serial Printer - Custom Wizard',
+    'version': '18.0.1.0.0',
+    'summary': 'Personalización de producto con vista previa en carrito',
+    'license': 'LGPL-3',
+    'category': 'Website/Website',
+    'depends': ['website_sale'],
+    'data': [
+        'views/customizer_page.xml',
+        'views/product_personalize_button.xml',
     ],
-    "assets": {
-        "web.assets_frontend": [
-            # --- lo que ya tenías ---
-            "serial_printer_custom_wizard/static/src/js/spw_button.js",
-            "serial_printer_custom_wizard/static/src/js/spw_logo_preview.js",
-            "serial_printer_custom_wizard/static/src/js/customizer.js",
-            "serial_printer_custom_wizard/static/src/js/ns300_colors.js",
-            "serial_printer_custom_wizard/static/src/js/spw_options.js",
-            "serial_printer_custom_wizard/static/src/js/spw_cart.js",
-            "serial_printer_custom_wizard/static/src/js/spw.js",
-            "serial_printer_custom_wizard/static/src/css/spw.css",
-            # --- AÑADIR ESTE ARCHIVO NUEVO (el que inyecta en el carrito) ---
-            "serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js",
+    'assets': {
+        'web.assets_frontend': [
+            # Colores / estilos
+            'serial_printer_custom_wizard/static/src/css/spw_colors.css',
+            'serial_printer_custom_wizard/static/src/css/customizer.css',
+            'serial_printer_custom_wizard/static/src/css/personalizar_preview.css',
+
+            # Botón y opciones en PDP
+            'serial_printer_custom_wizard/static/src/js/spw_button.js',
+            'serial_printer_custom_wizard/static/src/js/spw_options.js',
+
+            # Lógica customizer (si la usas además del inline)
+            'serial_printer_custom_wizard/static/src/js/spw_customizer.js',
+
+            # Inyección preview carrito/checkout
+            'serial_printer_custom_wizard/static/src/js/spw_cart_preview.inject.js',
         ],
     },
-    "installable": True,
-    "application": False,
+    'installable': True,
+    'application': False,
 }
