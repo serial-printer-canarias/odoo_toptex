31716f3abd2b3656937adf5f7518771fd21f7ec6
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-28 09:33:16 +0100

    Actualizar customizer.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/customizer.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/controllers/customizer.py
@@ -2,20 +2,44 @@
 from odoo import http
 from odoo.http import request
 
-class SpwCustomizerPage(http.Controller):
+class SPWCustomizer(http.Controller):
 
-    @http.route(['/spw/customizer'], type='http', auth='public', website=True, sitemap=False)
-    def spw_customizer(self, product_id=None, variant_id=None, **kw):
-        """Renderiza la página del customizer con imagen same-origin."""
-        product_t = request.env['product.template'].sudo().browse(int(product_id or 0))
-        vid = int(variant_id or (product_t.product_variant_id.id if product_t and product_t.product_variant_id else 0) or 0)
-        if vid:
-            img_src = f'/web/image/product.product/{vid}/image_1920'
-        else:
-            img_src = f'/web/image/product.template/{product_t.id}/image_1920' if product_t else ''
+    @http.route('/spw/customizer', type='http', auth='public', website=True)
+    def spw_customizer(self, product_id=None, product_template_id=None, variant_id=None, **kw):
+        """Renderiza la página del personalizador."""
+        env = request.env
+        Product = env['product.product'].sudo()
+        Template = env['product.template'].sudo()
+
+        variant = None
+        template = None
+
+        if variant_id:
+            variant = Product.browse(int(variant_id))
+            if variant and variant.exists():
+                template = variant.product_tmpl_id
+        elif product_id:
+            # por compatibilidad, product_id también puede ser la variante
+            v = Product.browse(int(product_id))
+            if v and v.exists():
+                variant = v
+                template = v.product_tmpl_id
+        elif product_template_id:
+            template = Template.browse(int(product_template_id))
+
+        if not template:
+            return request.not_found()
+
+        if not variant:
+            variant = template.product_variant_id
+
+        # Imagen base mostrada en la página
+        img_src = variant and f"/web/image/product.product/{variant.id}/image_1920" or f"/web/image/product.template/{template.id}/image_1920"
+
         values = {
-            'template': product_t,
-            'variant_id': vid,
+            'template': template,
+            'variant_id': variant.id if variant else False,
             'img_src': img_src,
         }
+        # ¡IMPORTANTE! usar render, no _render
         return request.render('serial_printer_custom_wizard.spw_customize_page', values)
