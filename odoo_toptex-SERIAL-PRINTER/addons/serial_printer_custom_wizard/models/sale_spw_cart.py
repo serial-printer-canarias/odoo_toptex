f3b712bdaee42a4c0bfb638de7810edc2e84c5cd
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-28 10:17:25 +0100

    Actualizar sale_spw_cart.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_cart.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
-import uuid
-from odoo import http
-from odoo.http import request
+from odoo import fields, models
 
-class SPWCartController(http.Controller):
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
-    @http.route(['/spw/add_to_cart'], type='json', auth='public', website=True, csrf=False)
-    def spw_add_to_cart(self, product_id, quantity=1, **kw):
-        # token único por personalización
-        spw_token = kw.get('spw_token') or uuid.uuid4().hex[:12]
-
-        order = request.website.sale_get_order(force_create=True)
-        res = order._cart_update(
-            product_id=int(product_id),
-            add_qty=float(quantity),
-            # ¡sin line_id para no forzar update de una línea existente!
-            spw_token=spw_token,
-        )
-
-        # Respondemos con el token y la line_id creada (útil para previews)
-        res['spw_token'] = spw_token
-        return res
+    spw_tech = fields.Char(string='SPW Técnica')
+    spw_svg_color = fields.Char(string='SPW Color SVG')
+    spw_notes = fields.Text(string='SPW Notas')
