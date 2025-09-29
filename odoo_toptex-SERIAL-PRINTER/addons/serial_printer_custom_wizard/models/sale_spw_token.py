bf3b07cd816e367dccd39fa759df86ca23ee662d
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-29 08:31:38 +0100

    Actualizar sale_spw_token.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_token.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/models/sale_spw_token.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import api, fields, models
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # Un identificador por personalización (no se copia ni se rellena solo)
-    spw_token = fields.Char(index=True, copy=False)
+    # token para diferenciar personalizaciones
+    spw_token = fields.Char(index=True)
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    # incluir spw_token en la búsqueda de línea similar
+    def _cart_find_product_line(self, product_id=None, line_id=None, **kwargs):
+        line = super()._cart_find_product_line(product_id=product_id, line_id=line_id, **kwargs)
+        token = kwargs.get('spw_token')
+        if token and line and getattr(line, 'spw_token', False) != token:
+            # forzamos "no match" para que Odoo cree NUEVA línea
+            return self.env['sale.order.line']
+        return line
