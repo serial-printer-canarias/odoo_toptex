a85743b66aadfeef96df9a692b3cf2c84d227cb9
serial-printer-canarias <marcbafaluy@gmail.com>
2025-09-29 09:13:25 +0100

    Actualizar __manifest__.py

--- odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_custom_wizard/__manifest__.py
@@ -9,6 +9,7 @@
         'views/customizer_page.xml',
         'views/product_personalize_button.xml',
         'views/spw_cart_preview_inject.xml',
+        'views/spw_color_palette_assets.xml',
     ],
     'assets': {
         'web.assets_frontend': [
