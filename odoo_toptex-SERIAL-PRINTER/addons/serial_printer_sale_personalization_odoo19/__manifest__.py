c2c0acf4449e1176e739e614b909a9d64f243b1b
serial-printer-canarias <marcbafaluy@gmail.com>
2026-05-07 10:41:55 +0100

    Add files via upload

---
+++ odoo_toptex-SERIAL-PRINTER/addons/serial_printer_sale_personalization_odoo19/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Serial Printer Sale Personalization",
+    "version": "19.0.1.6.0",
+    "summary": "Fichas de personalización textil desde presupuestos y facturas",
+    "author": "Serial Printer",
+    "website": "https://www.serial-printer.com",
+    "category": "Sales",
+    "license": "LGPL-3",
+    "depends": ["base", "sale_management", "account", "website", "portal", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/sequence.xml",
+        "views/personalization_views.xml",
+        "views/sale_order_views.xml",
+        "views/portal_templates.xml",
+        "reports/personalization_report.xml",
+        "reports/report_templates.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
